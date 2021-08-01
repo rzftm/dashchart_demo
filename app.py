399a62ea47fc9643995ae353ed82a828d0eae22f
Ronglin Zhao <rl.z@hotmail.com>
2021-08-01 10:37:22 +0100

    Change to relative path

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-
+import os
 import dash
 import dash_bootstrap_components as dbc
 
-folder_path = "H:\\_CODE\\dashchart_demo\\data\\"
+
+folder_path = os.getcwd() + "\\data\\"
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
 
 
--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 import pandas as pd
+import os
 
 def test_excel():
     df_fri = pd.read_excel('H:\\_CODE\\dashchart_demo\\data\\JPM_Global_PMI_Historica_2021-07-06_3793624.xlsx','Global PMI', skiprows=[0, 1, 2, 3, 5])
     print(df_fri.tail())
 
+def test_path():
+    rootpath = os.path.abspath(__file__)
+    directory = os.path.split(rootpath)[0]
+
 if __name__ == '__main__':
-    test_excel()
+    test_path()
